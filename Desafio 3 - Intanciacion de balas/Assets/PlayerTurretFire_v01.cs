34d557021817e072399c056e51d97ce586459490
JorgeBarton <jorge_barton@hotmail.com>
2022-07-23 18:54:47 -0300

    New

---
+++ Desafio 3 - Intanciacion de balas/Assets/PlayerTurretFire_v01.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurretFire_v01 : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject barrel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Fire()
+    {
+        Instantiate(bullet,barrel.transform);
+    }
+}
